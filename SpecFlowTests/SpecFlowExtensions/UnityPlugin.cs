a02b172ec92c0ddc43169f86ffe2dddd31b4e5e2
Felipe Jaramillo Gómez <felipe.jaramillo@ceiba.com.co>
2016-08-26 15:58:19 -0500

    UnityConfig

---
+++ SpecFlowTests/SpecFlowExtensions/UnityPlugin.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Practices.Unity;
+using SpecFlowTests.SpecFlowExtensions;
+using SpecFlowTests.Utils;
+using TechTalk.SpecFlow.Infrastructure;
+using TechTalk.SpecFlow.Plugins;
+
+[assembly: RuntimePlugin(typeof(UnityPlugin))]
+namespace SpecFlowTests.SpecFlowExtensions
+{
+    public class UnityPlugin:IRuntimePlugin
+    {
+        #region Implementation of IRuntimePlugin
+
+        public void Initialize(RuntimePluginEvents runtimePluginEvents, RuntimePluginParameters runtimePluginParameters)
+        {
+            runtimePluginEvents.CustomizeGlobalDependencies += (sender, args) =>
+            {
+                args.ObjectContainer.RegisterTypeAs<UnityBindingInstanceResolver, IBindingInstanceResolver>();
+            };
+
+            runtimePluginEvents.CustomizeScenarioDependencies += (sender, args) =>
+            {
+                args.ObjectContainer.RegisterFactoryAs<IUnityContainer>(() =>
+                {
+                    return DependenciesTest.GetTestContainer();
+                });
+            };
+        }
+
+        #endregion
+    }
+}
---
+++ SpecFlowTests/Utils/DependenciesTest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Practices.Unity;
+using SeleniumWebDriver.Drivers;
+using SpecFlowHelpers.Drivers;
+
+namespace SpecFlowTests.Utils
+{
+    public static class DependenciesTest
+    {
+        public static IUnityContainer GetTestContainer()
+        {
+            var container = new UnityContainer();
+
+            container.RegisterType<IDriver, FirefoxWebDriver>();
+
+            return container;
+        }
+    }
+}
