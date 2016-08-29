2ff56f66a02f51049c757dcdcc36b822c37f1dd4
Felipe Jaramillo Gómez <felipe.jaramillo@ceiba.com.co>
2016-08-29 08:37:06 -0500

    Unity

--- SpecFlowTests/Utils/DependenciesTest.cs
+++ SpecFlowTests/Utils/DependenciesTest.cs
@@ -5,19 +5,23 @@ using System.Text;
 using System.Threading.Tasks;
 using Microsoft.Practices.Unity;
 using SeleniumWebDriver.Drivers;
+using SpecFlow.Unity;
 using SpecFlowHelpers.Drivers;
+using TechTalk.SpecFlow;
 
 namespace SpecFlowTests.Utils
 {
     public static class DependenciesTest
     {
+        [ScenarioDependencies]
         public static IUnityContainer GetTestContainer()
         {
             var container = new UnityContainer();
 
             container.RegisterType<IDriver, FirefoxWebDriver>();
             container.RegisterType<ITestDriver, FirefoxTestBase>();
- 
+
+           // container.RegisterTypes(typeof(TestDependencies).Assembly.GetTypes().Where(t => Attribute.IsDefined(t, typeof(BindingAttribute))).ToArray()).SingleInstance();
             return container;
         }
     }
